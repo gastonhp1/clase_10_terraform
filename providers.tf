447d976f9fc007f67a13031e330f1d4adb8393b8
Gaston.Perez <gaston.perez@kyndryl.com>
2022-03-22 16:38:30 -0300

    new feature

--- providers.tf
+++ providers.tf
@@ -10,30 +10,30 @@
 
 terraform {
 
-# Le decimos que queremos:
-# a. la versión del binario de terraform mayor o igual a 0.12
+  # Le decimos que queremos:
+  # a. la versión del binario de terraform mayor o igual a 0.12
 
-    required_version = ">=0.12"
-    required_providers {
+  required_version = ">=0.12"
+  required_providers {
 
-        aws = {
-# Especificamos desde donde queremos descargar el binario:
+    aws = {
+      # Especificamos desde donde queremos descargar el binario:
 
-        source = "hashicorp/aws"
-# Le decimos que solo permitirá:
-# b. la versión del binario del provider 3.20.0 (con cierta restricción)
-        version = "~> 3.20.0"
+      source = "hashicorp/aws"
+      # Le decimos que solo permitirá:
+      # b. la versión del binario del provider 3.20.0 (con cierta restricción)
+      version = "~> 3.20.0"
 
     }
-}
+  }
 }
 
 # Declaramos la región donde queremos levantar nuestra infra
 
 provider "aws" {
-  region     = "${var.region}"
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+  region     = var.aws_region_id
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 
--- variables.tf
+++ variables.tf
@@ -1,42 +1,39 @@
 variable "access_key" {
-    type = string
-    default = "AKIAWB4P7DBD5HMJPLP5"
-    }
+  type    = string
+  default = "AKIAWB4P7DBDVDY2JMFC"
+}
 variable "secret_key" {
-    type = string
-    default = "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"
-    }
-variable "region" {
-    type = string
-    default = "us-east-1"
-    }
+  type    = string
+  default = "IFAIFLQzgcpuEgDuaf/A6sk9Z3yUBND34wxXePj/"
+}
 
 variable "aws_region_id" {
-    description = "la region"
-    type = string
-    default = "us-east-1"
+  description = "la region"
+  type        = string
+  default     = "us-east-1"
 
-    }
+}
 
 variable "main_vpc_cidr" {
 
-    description = "Nuestro Security Group"
-    type = string
-    default = "10.0.0.0/24"
+  description = "Nuestro Security Group"
+  type        = string
+  default     = "10.0.0.0/24"
 
 }
 
 variable "public_subnets" {
 
-    description = "subnet con acceso a internet"
-    type = string
-    default = "10.0.0.128/26"
+  description = "subnet con acceso a internet"
+  type        = string
+  default     = "10.0.0.128/26"
 }
 
 variable "private_subnets" {
 
-description = "subnet sin acceso a internet"
-type = string
-default = "10.0.0.192/26"
+  description = "subnet sin acceso a internet"
+  type        = string
+  default     = "10.0.0.192/26"
 
 }
+
